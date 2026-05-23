62b3dfd88eef12e933f739ab0849b96ae32cb592
ianyourgod <ianyourgod@gmail.com>
2026-05-23 16:47:07 -0500

    remove testing

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 const PenguinMod = require("./src/index.js");
 module.exports = PenguinMod;
-
-const api = new PenguinMod.PenguinModAPI();
-
-api.projects.getRandomProject().then((res) => console.log(res));
